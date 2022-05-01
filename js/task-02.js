4ff292b62f807cb39bfed510178fc9444efb94f7
YevsiukoVitaliy <93053290+YevsiukoVitaliy@users.noreply.github.com>
2022-05-01 21:08:34 +0300

    ds

--- js/task-02.js
+++ js/task-02.js
@@ -8,10 +8,20 @@ const ingredients = [
 ];
 const list = document.querySelector("#ingredients");
 
-const listItem = ingredients.map(elem => `<li><img class="item">${elem}
-</li>`).join("")
-list.insertAdjacentHTML("beforebegin",listItem)
 
+const listEl = (elem) => {
+  const listItem = document.createElement("li")
+  listItem.classList.add("item")
+  listItem.textContent = elem
+  return listItem
+}
+const createElement = ingredients.map(listEl)
+list.append(...createElement)
+
+
+// const listItem = ingredients.map(elem => `<li><img class="item">${elem}
+// </li>`).join("")
+// list.insertAdjacentHTML("beforebegin",listItem)
 
 
 // ingredients.forEach(elem => {
